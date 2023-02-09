9a1559097126d0f83bafa85d1e53022c8085f92f
Omkar <122523386+Omkar11Shirke@users.noreply.github.com>
2023-02-09 10:02:23 +0530

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,10 +1,22 @@
 #1
+name=input("Enter a Name: ")
+names=[]
+for char in name:
+  names.append(char)
+
+name=""
+for i in range(len(names)-1,-1,-1):
+  name=name+names[i]
+
+print(name)
+
+#2
 a = "My Name is Omkar"
 print ("String : " + a)
 res = len(a.split())
 print ("Total number of words : " + str(res))
 
-#2
+#3
 import re
 def check_pass(password):
   if len(password)  >= 8 and len(re.findall("[A-Z]",password))>0 and len(re.findall("[0-9]",password))>0:
@@ -15,7 +27,7 @@ def check_pass(password):
 password=input("Enter Password: ")
 check_pass(password)
 
-#3
+#4
 def isPalindrome(a):	
 	rev = ''.join(reversed(a))
 	if (a == rev):
